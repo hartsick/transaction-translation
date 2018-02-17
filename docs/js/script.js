39567364b2ec2ee08a9f2c9468d913a0744a4dec
Christa Hartsock <christa.hartsock@gmail.com>
2018-02-17 13:09:35 -0800

    Add labels to piechart

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,4 +1,64 @@
 window.onload = function() {
+    var addTooltips = function(path, dataset) {
+        var tooltip = d3.select('#chart')
+            .append('div')
+            .attr('class', 'tooltip');
+
+        tooltip.append('div')
+            .attr('class', 'label');
+
+        tooltip.append('div')
+            .attr('class', 'count');
+
+        tooltip.append('div')
+            .attr('class', 'percent');
+
+        path.on('mouseover', function(d) {
+            var total = d3.sum(dataset.map(function(d) {
+                return d.count;
+            }));
+            var percent = Math.round(1000 * d.data.count / total) / 10;
+            tooltip.select('.label').html(d.data.label);
+            tooltip.select('.percent').html(percent + '%');
+            tooltip.style('display', 'block');
+        });
+
+        path.on('mouseout', function() {
+            tooltip.style('display', 'none');
+        });
+    };
+
+    var createPieChart = function(dataset, colors) {
+        var width = 360;
+        var height = 360;
+        var radius = Math.min(width, height) / 2;
+        var color = d3.scaleOrdinal().range(colors);
+
+        var svg = d3.select('#chart')
+            .append('svg')
+            .attr('width', width)
+            .attr('height', height)
+            .append('g')
+            .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
+
+        var arc = d3.arc()
+            .innerRadius(0)
+            .outerRadius(radius);
+
+        var pie = d3.pie()
+            .value(function(d) { return d.count; })
+            .sort(null);
+
+        return svg.selectAll('path')
+            .data(pie(dataset))
+            .enter()
+            .append('path')
+            .attr('d', arc)
+            .attr('fill', function(d, i) {
+                return color(d.data.label);
+            });
+    };
+
     'use strict';
 
     var colorPalette = {
@@ -16,36 +76,9 @@ window.onload = function() {
     var colors = labels.map(function(label) { return colorPalette[label]; });
 
     var dataset = labels.map(function(label){
-        var randomNumber = Math.random() * 100;
-        return { label: label, count: randomNumber };
+        return { label: label, count: Math.random() };
     });
 
-    var width = 360;
-    var height = 360;
-    var radius = Math.min(width, height) / 2;
-    var color = d3.scaleOrdinal().range(colors);
-
-    var svg = d3.select('#chart')
-        .append('svg')
-        .attr('width', width)
-        .attr('height', height)
-        .append('g')
-        .attr('transform', 'translate(' + (width / 2) +  ',' + (height / 2) + ')');
-
-    var arc = d3.arc()
-        .innerRadius(0)
-        .outerRadius(radius);
-
-    var pie = d3.pie()
-        .value(function(d) { return d.count; })
-        .sort(null);
-
-    svg.selectAll('path')
-        .data(pie(dataset))
-        .enter()
-        .append('path')
-        .attr('d', arc)
-        .attr('fill', function(d, i) {
-            return color(d.data.label);
-        });
+    var path = createPieChart(dataset, colors);
+    addTooltips(path, dataset);
 };
